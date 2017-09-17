0cf418997ffefafd8f7f8bc6060758c4627ac6c5
SabrinaSabrinaroyRoy <sabrinaroy@SabrinaRoydeMacBook-Pro.local>
2017-09-17 08:05:26 +0800

    hi

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print('hello world')
+#/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import urllib.request
+
+url = "http://www.baidu.com"
+data = urllib.request.urlopen(url).read()
+print(data)
+with open('/Users/sabrinaroy/python_learn/test/note.txt', 'w') as f:
+	f.write(str(data))
